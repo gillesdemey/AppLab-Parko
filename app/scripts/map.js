d2564180bbe8317b056faeb01d563425cd628f1f
Michiel De Mey <de.mey.michiel@gmail.com>
2013-06-24 20:20:47 +0200

    Did some layout and centered parkings on map

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,20 +1,23 @@
+var parkings_objects = [];
 var map = L.mapbox.map('map', 'gillesdemey.map-vevzn2un');
 
 // Itterate parkings and add markers to the map
 var parkings = $.jStorage.get("parkings");
 if ( typeof parkings !== "undefined" ) {
   $(parkings).each(function() {
-
-    //console.log(this);
-
     var parking = new Parking(this);
-    console.log(parking);
 
     addMarker(parking);
   });
+
+  // Fit to view
+  var group = new L.featureGroup(parkings_objects);
+  console.log(group);
+  map.fitBounds(group.getBounds());
 }
 
 function addMarker(parking) {
   // Add the markers to the map
-  L.mapbox.markerLayer(parking.getGeoJson()).addTo(map);
+  var layer = L.mapbox.markerLayer(parking.getGeoJson()).addTo(map);
+  parkings_objects.push(layer);
 };
